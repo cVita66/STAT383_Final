b6caee05cbd51d337663af1f213c35e3d4c1ed79
Amanda <115050592+polarolo@users.noreply.github.com>
2023-12-04 20:15:50 -0500

    Update Amanda.R

--- Amanda.R
+++ Amanda.R
@@ -3,7 +3,7 @@
 ##########################
 ###Creation of a Multi-linear model for Alcohol, pH and Sulphites with respect to Quality
 
-##setwd("C:/Users/Amanda/Documents/GitHub/STAT383_Final")  
+setwd("C:/Users/Amanda/Documents/GitHub/STAT383_Final")  
 # Access to ggplot data visualization
 install.packages("ggplot2")         
 library(ggplot2)
@@ -31,28 +31,28 @@ redWineRandom <- WineProperties_Red[randomIndexes, ]
 #####
 
 # Creation of linear models -- Quality VS alcohol~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-red_alc_pH_sulphates_mlr <- lm(quality ~ alcohol + pH + `sulphates`, data = redWineRandom)
-print(summary(red_alc_pH_sulphates_mlr))
+red_alc_pH_sulphites_mlr <- lm(quality ~ alcohol + pH + `total sulfur dioxide`, data = redWineRandom)
+print(summary(red_alc_pH_sulphites_mlr))
 
-# Scatter Plot: Alcohol Vs. Sulphates
-ggplot(redWineRandom, aes(x=`sulphates` , y=alcohol, col=quality)) +
+# Scatter Plot: Alcohol Vs. Sulphites
+ggplot(redWineRandom, aes(x=`total sulfur dioxide` , y=alcohol, col=quality)) +
   geom_point(aes(size=pH)) +
   geom_smooth(method='lm', se=FALSE, color="red") +
   theme_minimal() +
-  labs(title="Red Wine: Alcohol vs. Sulphates", x="Sulphates",y="Alcohol")
+  labs(title="Red Wine: Alcohol vs. Sulphites", x="Sulphites",y="Alcohol")
 
-# Scatter Plot: Alcohol Vs. Sulphates
+# Scatter Plot: Alcohol Vs. Sulphites
 ggplot(redWineRandom, aes(x=quality , y=pH, col=alcohol)) +
-  geom_point(aes(size=`sulphates`)) +
+  geom_point(aes(size=`total sulfur dioxide`)) +
   theme_minimal() +
-  labs(title="Red Wine: Quality vs. pH", x="sulphates",y="Alcohol")
+  labs(title="Red Wine: Quality vs. pH", x="Sulphites",y="Alcohol")
 
-# Scatter Plot: Alcohol Vs. Sulphates
-ggplot(redWineRandom, aes(x=`sulphates` , y=alcohol, col=pH)) +
+# Scatter Plot: Alcohol Vs. Sulphites
+ggplot(redWineRandom, aes(x=`total sulfur dioxide` , y=alcohol, col=pH)) +
   geom_point() +
   geom_smooth(method='lm', se=FALSE, color="red") +
   facet_wrap(~quality) +
   theme_minimal() +
-  labs(title="Red Wine: Alcohol vs. Sulphates", x="Sulphates",y="Alcohol")
+  labs(title="Red Wine: Alcohol vs. Sulphites", x="Sulphites",y="Alcohol")
 
 
